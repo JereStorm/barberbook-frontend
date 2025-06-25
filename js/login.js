f8e3cae0bf440710a6e67a4a09d9acde43dfc7a4
braian <bbortolotti04@gmail.com>
2025-06-24 23:21:58 -0300

    se crea login-css y js y se estiliza el formulario

---
+++ js/login.js
@@ -0,0 +1,8 @@
+"use strict"
+const formLogin = document.getElementById("form-login");
+formLogin.addEventListener("submit",validarLogin());
+
+
+function validarLogin(){
+    console.log("hola mundo")
+}
