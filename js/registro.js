8d8ec0c7dc451888d5055c880c993a58127944e3
braian <bbortolotti04@gmail.com>
2025-06-26 16:50:31 -0300

    validacion-contraseñas-js

---
+++ js/registro.js
@@ -0,0 +1,35 @@
+"use strict"
+const formRegistro = document.getElementById("form-registro");
+formRegistro.addEventListener("submit",validarRegistro);
+
+
+function validarRegistro(){
+    
+    const email= document.getElementById("email").value.trim();
+    const password= document.getElementById("password").value;
+
+
+     // Validar formato de email
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        alert("El email no tiene un formato válido.");
+        return;
+    }
+
+    // Validar longitud de la contraseña
+    if (password.length < 6) {
+        alert("La contraseña debe tener al menos 6 caracteres.");
+        return;
+    }
+
+
+
+    const validarEmail= "peluquero@ejemplo.com";
+    const validarPassword= "123456";
+
+    if (email===validarEmail && password ===validarPassword) {
+        window.location.href="turnos.html";
+    } else {
+        alert ("Usuario o Contraseña incorrectos.")
+    }
+}
